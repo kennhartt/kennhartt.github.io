d00953d6b95e8bfd3006232771ce2d7dbd0f6c66
Kenny <kennyhsunarya@gmail.com>
2020-07-06 09:13:23 -0700

    fix js function

--- JS/function.js
+++ JS/function.js
@@ -2,20 +2,22 @@ var aboutItem = document.getElementById("aboutItem");
 var resumeItem = document.getElementById("resumeItem");
 var contactItem = document.getElementById("contactItem");
 
-$("#aboutItem").click(function () {
-  aboutItem.classList.add("active", "badge-info");
-  resumeItem.classList.remove("active", "badge-info");
-  contactItem.classList.remove("active", "badge-info");
-});
+$(document).ready(function () {
+  $("#aboutItem").click(function () {
+    aboutItem.classList.add("active", "badge-info");
+    resumeItem.classList.remove("active", "badge-info");
+    contactItem.classList.remove("active", "badge-info");
+  });
 
-$("#resumeItem").click(function () {
-  aboutItem.classList.remove("active", "badge-info");
-  resumeItem.classList.add("active", "badge-info");
-  contactItem.classList.remove("active", "badge-info");
-});
+  $("#resumeItem").click(function () {
+    aboutItem.classList.remove("active", "badge-info");
+    resumeItem.classList.add("active", "badge-info");
+    contactItem.classList.remove("active", "badge-info");
+  });
 
-$("#contactItem").click(function () {
-  aboutItem.classList.remove("active", "badge-info");
-  resumeItem.classList.remove("active", "badge-info");
-  contactItem.classList.add("active", "badge-info");
+  $("#contactItem").click(function () {
+    aboutItem.classList.remove("active", "badge-info");
+    resumeItem.classList.remove("active", "badge-info");
+    contactItem.classList.add("active", "badge-info");
+  });
 });
